e0cd8f4c1fb587df9a1dfe270dd76cbb5acbf77e
YNGTHEMODDER <yungalaxxxy@yandex.ru>
2025-03-12 19:38:43 +0300

    [0.5.0] updated GUID;

--- TestModForRepo/YNGgamevision.cs
+++ TestModForRepo/YNGgamevision.cs
@@ -7,7 +7,7 @@ namespace TestModForRepo
     [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
     public class TestMod: BaseUnityPlugin
     {
-        public const string pluginGuid = "yngprodaction.repo.testing";
+        public const string pluginGuid = "yngprodaction.repo.vision";
         public const string pluginName = "R.E.P.O. testing";
         public const string pluginVersion = "0.5.0";
 
