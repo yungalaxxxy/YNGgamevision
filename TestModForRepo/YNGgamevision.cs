4175fc3ce1cca2f181b00d46557a836c18180de4
YNGTHEMODDER <yungalaxxxy@yandex.ru>
2025-03-12 23:09:01 +0300

    [1.1.1] Bumped version;

--- TestModForRepo/YNGgamevision.cs
+++ TestModForRepo/YNGgamevision.cs
@@ -9,7 +9,7 @@ namespace YNGGANEVISION
     {
         public const string pluginGuid = "yngprodaction.repo.vision";
         public const string pluginName = "YNGGAMEVISION";
-        public const string pluginVersion = "1.1.0";
+        public const string pluginVersion = "1.1.1";
 
         public void Awake()
         {
